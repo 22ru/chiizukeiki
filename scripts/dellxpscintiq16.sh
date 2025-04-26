a455d413d5b06912e8b6f1ea691dc23ae87389ba
22ru <22ru@users.noreply.github.com>
2025-04-26 10:49:41 -0700

    bash script improved

--- scripts/dellxpscintiq16.sh
+++ scripts/dellxpscintiq16.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-xrandr --output DP-2 --scale 2x2
-xsetwacom set $(xinput list --id-only "Wacom Cintiq 16 Pen stylus") MapToOutput DP-2
-xsetwacom set $(xinput list --id-only "Wacom Cintiq 16 Pen eraser") MapToOutput DP-2
+for d in /sys/class/drm/*/edid; do
+	if cat $d | grep "Cintiq 16"; then
+		[[ "$d" =~ DP-[0-9] ]]
+		display=${BASH_REMATCH[0]}
+		echo "Cintiq 16 is on $display."
+	fi
+done
+
+if [[ -z "$display" ]]; then
+	echo "Cintiq 16 not found."
+	exit 1
+fi
+
+xrandr --output $display --scale 2x2
+xsetwacom set $(xinput list --id-only "Wacom Cintiq 16 Pen stylus") MapToOutput $display
+xsetwacom set $(xinput list --id-only "Wacom Cintiq 16 Pen eraser") MapToOutput $display
 
 sudo /home/hat/repo/chiizukeiki/chiizukeiki /home/hat/repo/chiizukeiki/default.map
